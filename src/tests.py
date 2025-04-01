f825542a1df909f0b26ed3bc94594bf9af16eef4
Paulo Guarino <pauloguarino97@gmail.com>
2025-04-01 20:55:18 -0300

    Changed tests.py

--- src/tests.py
+++ src/tests.py
@@ -1,31 +1,19 @@
-def tests():
-    dist = {
-        "0": 0.1,
-        "1": 0.8,
-    }
+from ti_modules import information
+import numpy as np
 
-    extended_dist = dict()
-    aux_dist = dict()
-    for simbol, probability in dist.items():
-        extended_dist[simbol] = probability
-        
-    n = 15
-    for i in range(n - 1):
-        aux_dist = dict()
-        for extended_simbol, extended_probability in extended_dist.items():
-            for simbol, probability in dist.items():
-                aux_dist[extended_simbol + simbol] = extended_probability*probability
-        extended_dist = dict()
-        for aux_simbol, aux_probability in aux_dist.items():
-            extended_dist[aux_simbol] = aux_probability
-    print(len(extended_dist))
-    
-    for i in range(n + 1):
-        d = dict()
-        for j in range(len(dist)**i):
-            d[j] = j
-    
-    print(len(d))
+def tests():
+    alphabet = ["0", "1"]
+    memory_pmf = np.array([[0.75, 0.25], [0.95, 0.05], [0, 1], [0.5, 0.5]])
+    estimated_pmf = np.array([[0, 1], [0.93, 0.0705], [0.0227, 0.977], [0.564, 0.436]])
+    memory_source = information.MemorySource((alphabet, memory_pmf))
+    extended_memory_source = information.MemorySource((alphabet, memory_pmf), 2)
+    extended_estimated_source = information.MemorySource((alphabet, estimated_pmf), 2)
+    print(memory_source)
+    print(extended_memory_source)
+    print(extended_memory_source.unconditional_source())
+    print(extended_estimated_source)
+    print(extended_estimated_source.unconditional_source())
+    print(np.array([extended_memory_source.probability(i) - extended_estimated_source.probability(i) for i in range(4)]))
 
 
 if __name__ == "__main__":
